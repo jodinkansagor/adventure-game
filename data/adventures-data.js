8bbe5abe166c6996d83f815fbf78b23e08984234
JodiBonJodi <joedvice@gmail.com>
2019-10-16 17:40:36 -0700

    made some data

---
+++ data/adventures-data.js
@@ -0,0 +1,94 @@
+
+
+const practice = {
+    id: 'practice',
+    title: 'Your first practice!',
+    map: {
+        top: XXX%,
+        left: XXX%
+    },
+    image: "",
+    audio: "",
+    description: "You show up to the first day of practice on a new time. You dont know any of the strategies. What do you do?",
+    choices: [{
+        id: 'pretend',
+        description: 'Pretend like you know what\'s going on and play along',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'stop',
+        description: 'Stop practice and make them go over the strategy.',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'ask',
+        description: 'Find your closest friend on the bench and quietly see if they can help you out.',
+        result: '',
+        bp: X,
+        cp: X
+    }]
+};
+
+const party = {
+    id: 'party',
+    title: 'After-party time!',
+    map: {
+        top: XXX%,
+        left: XXX%
+    },
+    image: "",
+    audio: "",
+    description: 'It\'s time for a bout after-party. Your roller derby idol is there. What do you do?',
+    choices: [{
+        id: 'interrupt',
+        description: 'Interrupt their conversation and tell them how amazing you think they are.',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'creep',
+        description: 'Get creepy about it and \"happen\" to be in every conversation they are in.',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'stay-cool',
+        description: 'Just hang out with your friends. Maybe they will come over and say hi!',
+        result: '',
+        bp: X,
+        cp: X
+    }]
+};
+
+const bout = {
+    id: 'bout',
+    title: 'It\'s BOUT DAY!',
+    map: {
+        top: XXX%,
+        left: XXX%
+    },
+    image: "",
+    audio: "",
+    description: 'Bout day is upon us. What do you eat to get yourself ready for the big event?',
+    choices: [{
+        id: 'burger',
+        description: 'One small burger from your favorite local fast food establishment. Carbs, fat, protein - in one little package.',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'nothing',
+        description: 'Nothing. You are way too nervous to eat.',
+        result: '',
+        bp: X,
+        cp: X
+    }, {
+        id: 'everything',
+        description: 'All of it - eggs, bacon, toast, fruit, granola, sausage, pizza. What don\'t you eat?,
+        result: '',
+        bp: X,
+        cp: X
+    }]
+};
